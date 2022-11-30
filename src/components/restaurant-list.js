c87399537e9647e64c5b3a8bf5564082e061c67f
Farul Ivan <farulivan@gmail.com>
2022-11-30 14:45:04 +0700

    adding onclick to restaurants item instead of anchor to the restaurant name

--- src/components/restaurant-list.js
+++ src/components/restaurant-list.js
@@ -24,12 +24,12 @@ class RestaurantList extends HTMLElement {
         ({
           id, name, pictureId, city, description, rating,
         }) => `
-      <div class="restaurant__item" id=${id}>
+      <div class="restaurant__item" id=${id} onclick="location.href='/#/detail/${id}';">
       <div class="restaurant__image">
           <img src="${CONFIG.BASE_IMAGE_URL}small/${pictureId}" alt="picture of ${name}" />
       </div>
       <div class="restaurant__info">
-          <p class="restaurant__name"><a href="/#/detail/${id}">${name}</a></p>
+          <p class="restaurant__name">${name}</p>
           <p class="restaurant__city">${city}</p>
           <p class="restaurant__description">${description}</p>
           <p class="restaurant__rating">⭐ <span>${rating}</span>/5</p>
